0c4f1475a299778032cf7d8d4d7e5d02e2608c94
singhbraj <42037630+singhbraj@users.noreply.github.com>
2020-05-20 22:42:13 +0530

    Updated blog.js

--- blog.js
+++ blog.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-Parser');
 const path = require('path');
 const app=express();
+const methodOverride=require('method-override');
 app.set('view engine','ejs');
 app.set(path.join(__dirname,'../','views'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+// app.use(express.static(__dirname + '/Public'));
 
 // config with mongodb
 
@@ -24,14 +28,17 @@ let Blog=mongoose.model('blog',blogSchema);
 
 /// RestFull Routes
 
-Blog.create({
-    title:"puppy",
-    image:"https://unsplash.com/photos/T-0EW-SEbsE",
-    body:"Trustworthy dog!!"
-});
+// Blog.create({
+//     title:"Rusty",
+//     image:"https://photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c7d2772d09e4ac05c_1280.jpg&user=Pexels",
+//     body:"Sanskari dog!!"
+// });
 app.get('/',(req,res)=>{
      res.redirect('/blog');
 });
+ 
+   // Index Route   
+
 app.get('/blog',(req,res)=>{
     Blog.find({},(err,blogs)=>{
         if(err)
@@ -44,6 +51,97 @@ app.get('/blog',(req,res)=>{
         }
     });
     //res.render("blog_index");
+});
+ 
+ // New Route
+ 
+app.get('/blog/new',(req,res)=>{
+    res.render('blog_new');
+});
+
+// Create Route
+
+app.post('/blog',(req,res)=>{
+    // let title=req.body.title;
+    // let image=req.body.title;
+    // let body=req.body.title;
+    // let newBlog={title:title, image:image, body:body};
+    Blog.create(req.body.blog,(err,newBlog)=>{
+        if(err)
+        {
+            res.render(blog_new);
+        }
+        else
+        {
+            res.redirect('/blog');
+        }
+
+    });
+});
+  
+//   Show Route
+
+app.get('/blog/:id',(req,res)=>{
+       Blog.findById(req.params.id,(err,referBlog)=>{
+           if(err)
+           {
+               res.redirect('/blog');
+           }
+           else
+           {
+            res.render('blog_show',{blog:referBlog});
+           }
+   
+      });
+});
+
+// EDIT Route
+
+app.get('/blog/:id/edit',(req,res)=>{
+
+    Blog.findById(req.params.id,(err,referBlog)=>{
+        if(err)
+        {
+            res.redirect('/blog');
+        }
+        else
+        {
+         res.render('blog_edit',{blog:referBlog});
+        }
+    });
+});
+
+//    Update Route
+
+app.put("/blog/:id",(req,res)=>{
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err,updatedBlog)=>{
+        if(err)
+        {
+            res.redirect('/blog');
+        }
+        else
+        {
+            //res.send("Hello i am update!!");
+         res.redirect('/blog/' + req.params.id);
+        }
+});
+});
+
+
+//  Delete Route
+
+app.delete('/blog/:id',(req,res)=>{
+// res.send('You have reached the destroy route!!'); 
+Blog.findByIdAndRemove(req.params.id, (err)=>{
+    if(err)
+    {
+        res.redirect('/blog');
+    }
+    else
+    {
+        res.redirect('/blog');
+    }
+})
 });
 
 app.listen(3000);
